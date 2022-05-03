5a4a15eb39208b47874dbbc93aa40e23d3a52587
Aisha Kimpson <akimp313@gmail.com>
2022-05-03 19:58:00 -0400

    added function passwordLength

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ const numbersString = '0123456789';
 const lowercaseString = 'abcdefghijklmnopqrstuvwxyz';
 const specialCharacterString = '!@#$%^&*()';
 const uppercaseString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function passwordLengthPrompt() {
+    const passwordLength = window.prompt('Enter Password Length between 8-128');
+
+    return passwordLength;
+}
+
