5ec5e0f094c8e16300ed9cf2c11a0e9d2df74644
Aisha Kimpson <akimp313@gmail.com>
2022-05-04 00:49:34 -0400

    added functions numeric / special

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate");
 
-const numbersString = '0123456789';
-const lowercaseString = 'abcdefghijklmnopqrstuvwxyz';
-const specialCharacterString = '!@#$%^&*()';
-const uppercaseString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbersString = "0123456789";
+const lowercaseString = "abcdefghijklmnopqrstuvwxyz";
+const specialCharacterString = "!@#$%^&*()";
+const uppercaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 function passwordLengthPrompt() {
-    const passwordLength = window.prompt('Enter Password Length between 8-128');
+  const passwordLength = window.prompt("Enter Password Length between 8-128");
 
-    return passwordLength;
+  return passwordLength;
+}
+
+function upperaseCharactersPrompt() {
+  const uppercaseCharacters = window.prompt(
+    "Would you like uppercase letters? Enter 'YES' or 'NO' to make your choice"
+  );
+  if (uppercaseCharacters === "YES" || uppercaseCharacters === "yes") {
+    return uppercaseString;
+  } else {
+    return;
+  }
 }
 
 function lowercaseCharactersPrompt() {
-    const lowercaseCharacters = window.prompt(
-        "Would you like lowercase lettercase? Enter 'YES' or 'NO' to make your choice"
+  const lowercaseCharacters = window.prompt(
+    "Would you like lowercase letters? Enter 'YES' or 'NO' to make your choice"
+  );
+  if (lowercaseCharacters === "YES" || lowercaseCharacters === "yes") {
+    return lowercaseString;
+  } else {
+    return;
+  }
+}
+
+function numericCharactersPrompt() {
+    const numericCharacters = window.prompt(
+        "Would you like number options? Enter 'YES' or 'NO' to make your choice"
     );
-    if (lowercaseCharacters === 'YES' || lowercaseCharacters === 'yes') {
-        return lowercaseString;
+    if(numericCharacters === 'YES' || numericCharacters === 'yes' {
+        return numbersString;
     } else {
         return;
     }
 }
+
+function specialCharactersPrompt( ) { 
+    const specialCharacters = window.prompt(
+    "Would you like special characters? Enter 'YES' or 'NO' to make your choice"
+    );
+if (specialCharacters ==='YES' || specialCharacters === 'yes') {
+    return specialCharacters
+    } else {
+      return;  
+    }
+}    
